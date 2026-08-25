0f2f7329e85d6745c45907a1d7bdb935d3112d6d
Mohammad Parsa Valaei <85112623+AzizParsa89@users.noreply.github.com>
2026-08-25 11:21:23 +0330

    Improve the MealDetails Screen

--- lib/screens/meal_details.dart
+++ lib/screens/meal_details.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:meals/models/meal.dart';
 import 'package:transparent_image/transparent_image.dart';
 
-
 class MealDetailsScreen extends StatelessWidget {
   const MealDetailsScreen({super.key, required this.meal});
 
@@ -12,25 +11,61 @@ class MealDetailsScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text(meal.title)),
-      body: Column(
-        children: [
-          FadeInImage(
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            FadeInImage(
               placeholder: MemoryImage(kTransparentImage),
               image: NetworkImage(meal.imageUrl),
               imageErrorBuilder: (context, error, stackTrace) {
                 return const Center(
-                  child: Icon(
-                    Icons.broken_image,
-                    size: 50,
-                    color: Colors.white,
-                  ),
+                  child: Icon(Icons.broken_image, size: 50, color: Colors.white),
                 );
               },
               fit: BoxFit.cover,
               height: 300,
               width: double.infinity,
             ),
-        ],
+            const SizedBox(height: 14),
+            Text(
+              'Ingredients',
+              style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                color: Theme.of(context).colorScheme.primary,
+              ),
+            ),
+            const SizedBox(height: 14),
+        
+            for (final ingredient in meal.ingredients)
+              Text(
+                ingredient,
+                style: Theme.of(context).textTheme.bodyMedium!.copyWith(
+                  color: Theme.of(context).colorScheme.onSurface,
+                ),
+              ),
+            const SizedBox(height: 24),
+            Text(
+              'Steps',
+              style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                color: Theme.of(context).colorScheme.primary,
+              ),
+            ),
+            const SizedBox(height: 14),
+            for (final step in meal.steps)
+              Padding(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 12,
+                  vertical: 8,
+                ),
+                child: Text(
+                  step,
+                  textAlign: TextAlign.center,
+                  style: Theme.of(context).textTheme.bodyMedium!.copyWith(
+                    color: Theme.of(context).colorScheme.onSurface,
+                  ),
+                ),
+              ),
+          ],
+        ),
       ),
     );
   }
