0fcbe39fca06cc049a1eb1f55eece8d8fbeb6152
Mohammad Parsa Valaei <85112623+AzizParsa89@users.noreply.github.com>
2026-08-30 10:42:58 +0330

    Apply filters and pass available meals
    Connect filter UI to tabs and categories: add kInitialFilters and _selectedFilters in TabsScreen, compute availableMeals from dummyMeals using the selected filters, and pass that list into CategoriesScreen. Make FiltersScreen accept currentFilters and initialize its local state in initState. When returning from FiltersScreen, update _selectedFilters (with a null-safe fallback). Small wiring and state-passing changes to enable filtering of displayed meals.

--- lib/screens/tabs.dart
+++ lib/screens/tabs.dart
@@ -1,9 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:meals/data/dummy_data.dart';
 import 'package:meals/models/meal.dart';
 import 'package:meals/screens/categories.dart';
 import 'package:meals/screens/filters.dart';
 import 'package:meals/screens/meals.dart';
 import 'package:meals/widgets/main_drawer.dart';
+
+const kInitialFilters = {
+  Filter.glutenFree: false,
+  Filter.lactoseFree: false,
+  Filter.vegan: false,
+  Filter.vegetarian: false,
+};
 
 class TabsScreen extends StatefulWidget {
   const TabsScreen({super.key});
@@ -15,6 +23,7 @@ class TabsScreen extends StatefulWidget {
 }
 
 class _tabsScreenState extends State<TabsScreen> {
+  Map<Filter,bool> _selectedFilters = kInitialFilters;
   int _selectedPageIndex = 0;
   final List<Meal> _favoriteMeals = [];
   void _showInfoMessage(String text) {
@@ -49,18 +58,36 @@ class _tabsScreenState extends State<TabsScreen> {
     Navigator.of(context).pop();
 
     if (identifier == 'filters') {
-       final result = await Navigator.of(
-        context,
-      ).push<Map<Filter,bool>>(MaterialPageRoute(builder: (ctx) => FiltersScreen()));
-    print(result);
-
+      final result = await Navigator.of(context).push<Map<Filter, bool>>(
+        MaterialPageRoute(builder: (ctx) => FiltersScreen(currentFilters:_selectedFilters ,)),
+      );
+      setState(() {
+      _selectedFilters = result ?? kInitialFilters;
+        
+      });
     }
   }
 
   @override
   Widget build(BuildContext context) {
+    final availableMeals = dummyMeals.where((meal) {
+      if (_selectedFilters[Filter.glutenFree]! && !meal.isGlutenFree) {
+        return false;
+      }
+      if (_selectedFilters[Filter.lactoseFree]! && !meal.isLactoseFree) {
+        return false;
+      }
+      if (_selectedFilters[Filter.vegetarian]! && !meal.isVegetarian) {
+        return false;
+      }
+      if (_selectedFilters[Filter.vegan]! && !meal.isVegan) {
+        return false;
+      }
+      return true;
+    }).toList();
     Widget activePage = CategoriesScreen(
       onToggleFavorite: _toggleMealFavoriteStatus,
+      availableMeals: availableMeals,
     );
     var activePageTitle = 'Pick Your Category';
 
